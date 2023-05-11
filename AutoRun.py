2345845d77b69b41a0b419c76ba3448db0fdeb1e
72PikaChu72 <72vladvlad72@gmail.com>
2023-05-11 20:31:28 +0500

    Автозагрузка расписания каждые 3 часа

--- AutoRun.py
+++ AutoRun.py
@@ -2,21 +2,27 @@ import ScheduleBot
 import sql
 import logging
 import parsKCPT
+import threading
+import time
 
-
-print('Загрузка данных с сайта')
-#parsKCPT.Download()
+def Downloader():
+    while True:
+      print('Загрузка данных с сайта')
+      logging.info('Загрузка данных с сайта')
+      parsKCPT.Download()
+      time.sleep(3*60*60)
 logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w', format='%(asctime)s - %(levelname)s - %(message)s')
 logging.info('Старт бота')
 sql.Database.StartDatabase()
+downloader = threading.Thread(target = Downloader)
+downloader.start()
 logging.info('Бот запущен')
 print('Бот запущен')
-
 while True:
   try:
         ScheduleBot.StartBot()
   except Exception as e:
        print(e)
        logging.info("***ERROR***: Произошло исключение: %s", str(e))
-
+downloader.join()
         

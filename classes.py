989715c8e4610e8defd76ce737e0509f46aa1fa0
Arondhan <lifecrusher7@gmail.com>
2023-05-11 19:00:34 +0300

    пофиксил таблицу

--- classes.py
+++ classes.py
@@ -11,18 +11,33 @@ import numpy
 # Работает, лайк за то что все мега просто, как варик можно прикрутить кастомизацию
 @staticmethod
 def getScheduleAsImg(Schedule):
-    header = ["Урок", "Предмет", "Кабинет", "Преподаватель", "Время"]
+    header = ["Урок", "Предмет", "Каб.", "Преподаватель", "Время"]
     table_data = []
+    prev_urok = None
+    max_width = 20
+    max_width_kab = 3
+
     for urok in Schedule.Uroki:
-        max_width = 20
+        if prev_urok is not None:
+        # Расчет разницы в уроках
+            num_diff = urok.Number - prev_urok.Number - 1
+
+            if num_diff > 0:
+            # Добавление пустых строк в соответствии с разницей
+                for _ in range(num_diff):
+                    empty_row = [prev_urok.Number + 1] + [""] * 4
+                    table_data.append(empty_row)
+
         name_lines = textwrap.wrap(urok.Name, max_width)
-        kabinet_lines = textwrap.wrap(str(urok.Kabinet), max_width)
+        kabinet_lines = textwrap.wrap(str(urok.Kabinet), max_width_kab)
         prepod_lines = textwrap.wrap(str(urok.Prepod), max_width)
-        row = [urok.Number, name_lines, kabinet_lines, prepod_lines, GetUrokTime(Schedule.Date,urok.Number)]
+        row = [urok.Number, name_lines, kabinet_lines, prepod_lines, GetUrokTime(Schedule.Date, urok.Number)]
         table_data.append(row)
 
+        prev_urok = urok
+
     y = 30 * sum([max([len(row[i]) for i in range(1, 4)]) for row in table_data]) + len(table_data) + 1
-    image = Image.new('RGB', (1350, y + 350), color=(255, 255, 255))
+    image = Image.new('RGB', (680, y + 350), color=(255, 255, 255))
     draw = ImageDraw.Draw(image)
 
     font = ImageFont.truetype('arial.ttf', size=18)
@@ -30,21 +45,32 @@ def getScheduleAsImg(Schedule):
     x = 10
     y = 10
 
-    cell_width = 265
+    cell_width = [60, 265, 50, 180, 110]  # Ширина ячеек для каждого столбца
     cell_height = 30
 
-    for i in range(len(header)):
-        draw.rectangle((x, y, x + cell_width, y + 30), outline=(0, 0, 0))
-        draw.text((x + 5, y + 5), header[i], font=font, fill=(0, 0, 0))
-        x += cell_width
+    logo_image = Image.open("Logo.png")
+    logo_width, logo_height = logo_image.size
+    new_logo_width = int(logo_width * 0.9)
+    new_logo_height = int(logo_height * 0.9)
+    logo_image = logo_image.resize((new_logo_width, new_logo_height))
+    x_logo = (image.width - new_logo_width) // 2
+    y_logo = (image.height - new_logo_height) // 2
+    image.paste(logo_image, (x_logo, y_logo))
 
+    for i in range(len(header)):
+        header_width, header_height = draw.textsize(header[i], font=font)
+        text_x = x + (cell_width[i] - header_width) // 2
+        text_y = y + (cell_height - header_height) // 2
+        draw.rectangle((x, y, x + cell_width[i], y + cell_height), outline=(0, 0, 0))
+        draw.text((text_x, text_y), header[i], font=font, fill=(0, 0, 0))
+        x += cell_width[i]
     x = 10
     y += cell_height
 
     for row in table_data:
         max_lines = max([len(row[i]) for i in range(1, 4)])
         for i in range(len(row)):
-            draw.rectangle((x, y, x + cell_width, y + cell_height * (max_lines+1)), outline=(0, 0, 0))
+            draw.rectangle((x, y, x + cell_width[i], y + cell_height * (max_lines + 1)), outline=(0, 0, 0))
             if i == 0:
                 draw.text((x + 5, y + 5), str(row[i]), font=font, fill=(0, 0, 0))
             elif i == 1 or i == 2 or i == 3:
@@ -52,11 +78,11 @@ def getScheduleAsImg(Schedule):
                     draw.text((x + 5, y + 5 + j * cell_height), line, font=font, fill=(0, 0, 0))
             else:
                 draw.text((x + 5, y + 5), str(row[i]), font=font, fill=(0, 0, 0))
-            x += cell_width
+            x += cell_width[i]
         x = 10
-        y += cell_height * (max_lines+1)
-
+        y += cell_height * (max_lines + 1)
     return image
+
 
 
 
